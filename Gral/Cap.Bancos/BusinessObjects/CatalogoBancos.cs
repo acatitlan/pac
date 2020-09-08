b272903de85a68e4d0eb19518bb3b0d2049b1e60
acatitlan <69309184+acatitlan@users.noreply.github.com>
2020-09-08 05:07:19 -0500

    Add files via upload

---
+++ Gral/Cap.Bancos/BusinessObjects/CatalogoBancos.cs
@@ -0,0 +1,85 @@
+﻿#region Copyright (c) 2017-2020 cjlc
+/*
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++)
+{                                                                   }
+{     cjlc Cap control administrativo personal                      }
+{                                                                   }
+{     Copyrigth (c) 2017-2020 cjlc                                  }
+{     Todos los derechos reservados                                 }
+{                                                                   }
+{*******************************************************************}
+ */
+#endregion
+
+using DevExpress.Xpo;
+using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using Cap.Generales.BusinessObjects.Object;
+
+namespace Cap.Bancos.BusinessObjects
+{
+    [NavigationItem("Bancos")]
+    [DefaultProperty("Nmbr")]
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class CatalogoBancos : PObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        public CatalogoBancos(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        //private string _PersistentProperty;
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
+        //public string PersistentProperty {
+        //    get { return _PersistentProperty; }
+        //    set { SetPropertyValue("PersistentProperty", ref _PersistentProperty, value); }
+        //}
+
+        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
+        //public void ActionMethod() {
+        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
+        //    this.PersistentProperty = "Paid";
+        //}
+
+
+
+        private string mNmbr;
+        [RuleRequiredField("RuleRequiredField for CatalogoBancos.Nmbr", DefaultContexts.Save, "Debe capturar el Nombre", SkipNullOrEmptyValues = false)]
+        [XafDisplayName("Nombre")]
+        [Size(80)]
+        public string Nmbr
+        {
+            get { return mNmbr; }
+            set { SetPropertyValue("Nmbr", ref mNmbr, value); }
+        }
+
+        private ETIPOBANCOS mTp;
+        [XafDisplayName("Tipo")]
+        public ETIPOBANCOS Tp
+        {
+            get { return mTp; }
+            set { SetPropertyValue("Tp", ref mTp, value); }
+        }
+    }
+
+    public enum ETIPOBANCOS
+    {
+        /// <summary>
+        /// Es el que uso en Movientos ?
+        /// </summary>
+        FormaPago
+    }
+}
