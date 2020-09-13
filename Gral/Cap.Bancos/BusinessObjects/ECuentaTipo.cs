97cf3c756ca1f92d354473e7dfe4ec319fa32eb5
acatitlan <69309184+acatitlan@users.noreply.github.com>
2020-09-13 12:12:29 -0500

    Add files via upload

---
+++ Gral/Cap.Bancos/BusinessObjects/ECuentaTipo.cs
@@ -0,0 +1,34 @@
+﻿#region Copyright (c) 2015-2020 cjlc
+/*
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++)
+{                                                                   }
+{     cjlc Cap control administrativo personal                      }
+{                                                                   }
+{     Copyrigth (c) 2015-2020 cjlc                                  }
+{     Todos los derechos reservados                                 }
+{                                                                   }
+{*******************************************************************}
+ */
+#endregion
+
+using DevExpress.ExpressApp.DC;
+
+namespace Cap.Bancos.BusinessObjects
+{
+    public enum ECuentaTipo
+    {
+        [XafDisplayName("Crédito")]
+        Credito = 0,
+        [XafDisplayName("Débito")]
+        Debito = 1,
+        Maestra = 2,
+        [XafDisplayName("Inversión")]
+        Inversion = 3,
+        Caja = 4,
+        Afore = 5,
+        Ninguna = 6,
+        [XafDisplayName("Inválida")]
+        Invalida = -1,
+        Todos = 7
+    }
+}
