62f0457b6b50e83bfd9412cba7db40166fa9b33c
acatitlan <69309184+acatitlan@users.noreply.github.com>
2020-09-15 08:45:45 -0500

    Add files via upload

---
+++ Gral/Cap.Bancos/BusinessObjects/EPagoTipo.cs
@@ -0,0 +1,29 @@
+﻿#region Copyright (c) 2014-2020 cjlc
+/*
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++)
+{                                                                   }
+{     cjlc Cap control administrativo personal                      }
+{                                                                   }
+{     Copyrigth (c) 2014-2020 cjlc                                  }
+{     Todos los derechos reservados                                 }
+{                                                                   }
+{*******************************************************************}
+ */
+#endregion
+
+using DevExpress.ExpressApp.DC;
+
+namespace Cap.Bancos.BusinessObjects
+{
+    //#region Pago tipo
+    public enum EPagoTipo
+    {
+        Efectivo = 1,
+        [XafDisplayName("Débito")]
+        Debito = 2,
+        [XafDisplayName("Crédito")]
+        Credito = 3,
+        Cheque = 4
+    }
+    //#endregion
+}
