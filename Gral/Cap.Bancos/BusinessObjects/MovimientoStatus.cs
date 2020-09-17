bc1b52eb7b7498ba21dd58d5a03edc3b3365c0a7
acatitlan <69309184+acatitlan@users.noreply.github.com>
2020-09-17 04:54:19 -0500

    Add files via upload

---
+++ Gral/Cap.Bancos/BusinessObjects/MovimientoStatus.cs
@@ -0,0 +1,26 @@
+﻿#region Copyright (c) 2014-2020 cjlc
+/*
+{+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++)
+{                                                                   }
+{     cjlc Cap control administrativo personal                      }
+{                                                                   }
+{     Copyrigth (c) 2014-2020 cjlc                                  }
+{     Todos los derechos reservados                                 }
+{                                                                   }
+{*******************************************************************}
+ */
+#endregion
+
+using DevExpress.ExpressApp.DC;
+
+namespace Cap.Bancos.BusinessObjects
+{
+    public enum MovimientoStatus
+    {
+        Firme = 1,
+        [XafDisplayName("Tránsito")]
+        Transito = 2,
+        Cancelado = 3,
+        Conciliado = 4
+    }
+}
