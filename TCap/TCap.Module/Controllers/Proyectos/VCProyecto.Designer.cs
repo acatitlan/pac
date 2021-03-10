341c4a6696b3d24f00f12562ff5b90c02766d0bd
acatitlan <69309184+acatitlan@users.noreply.github.com>
2021-03-10 13:29:04 -0600

    Add files via upload

---
+++ TCap/TCap.Module/Controllers/Proyectos/VCProyecto.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace TCap.Module.Controllers.Proyectos
+{
+    partial class VCProyecto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleActionCxc = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.simpleActionAcptd = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.simpleActionCncld = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.simpleActionTrmnd = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.simpleActionEnPrcs = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.popupWindowShowActionNwTsk = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            this.popupWindowShowActionPryctCncld = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            this.simpleActionImprmrCtzcn = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.simpleActionEdtrCtzcn = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            this.popupWindowShowActionEnvrCrr = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            // 
+            // simpleActionCxc
+            // 
+            this.simpleActionCxc.Caption = "Crea la Cxc";
+            this.simpleActionCxc.ConfirmationMessage = "Está seguro de Crear la Cxc ?";
+            this.simpleActionCxc.Id = "f2fd6aae-bc6c-44fc-982f-1dc8bdf92dd8";
+            this.simpleActionCxc.ToolTip = null;
+            this.simpleActionCxc.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionCxc_Execute);
+            // 
+            // simpleActionAcptd
+            // 
+            this.simpleActionAcptd.Caption = "Proyecto Aceptado";
+            this.simpleActionAcptd.Category = "RecordEdit";
+            this.simpleActionAcptd.ConfirmationMessage = "Está seguro de Activar el Proyecto ?";
+            this.simpleActionAcptd.Id = "eeae70fa-34f2-43aa-a307-27bd02475115";
+            this.simpleActionAcptd.ToolTip = null;
+            this.simpleActionAcptd.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionAcptd_Execute);
+            // 
+            // simpleActionCncld
+            // 
+            this.simpleActionCncld.Caption = "Proyecto Cancelado";
+            this.simpleActionCncld.ConfirmationMessage = "Está seguro de Cancelar el Proyecto ?";
+            this.simpleActionCncld.Id = "8dcba1ab-38af-4f64-8b7b-6d0d7b938eac";
+            this.simpleActionCncld.ToolTip = null;
+            this.simpleActionCncld.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionCncld_Execute);
+            // 
+            // simpleActionTrmnd
+            // 
+            this.simpleActionTrmnd.Caption = "Proyecto Terminado";
+            this.simpleActionTrmnd.ConfirmationMessage = "Está seguro de Terminar el Proyecto?";
+            this.simpleActionTrmnd.Id = "66f69e8c-e788-4e6d-bedf-c8fa85ffffbe";
+            this.simpleActionTrmnd.ToolTip = null;
+            this.simpleActionTrmnd.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionTrmnd_Execute);
+            // 
+            // simpleActionEnPrcs
+            // 
+            this.simpleActionEnPrcs.Caption = "En Proceso";
+            this.simpleActionEnPrcs.ConfirmationMessage = "Está seguro de Poner en Marcha el Proyecto?";
+            this.simpleActionEnPrcs.Id = "063eda01-b38d-4377-b429-1dd7579a410f";
+            this.simpleActionEnPrcs.ToolTip = null;
+            this.simpleActionEnPrcs.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionEnPrcs_Execute);
+            // 
+            // popupWindowShowActionNwTsk
+            // 
+            this.popupWindowShowActionNwTsk.AcceptButtonCaption = "Agregar";
+            this.popupWindowShowActionNwTsk.CancelButtonCaption = null;
+            this.popupWindowShowActionNwTsk.Caption = "Agregar Tarea";
+            this.popupWindowShowActionNwTsk.Category = "RecordEdit";
+            this.popupWindowShowActionNwTsk.ConfirmationMessage = null;
+            this.popupWindowShowActionNwTsk.Id = "77fcb4c5-200a-4df2-a7da-5ec4962bf472";
+            this.popupWindowShowActionNwTsk.ToolTip = null;
+            this.popupWindowShowActionNwTsk.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionNwTsk_CustomizePopupWindowParams);
+            this.popupWindowShowActionNwTsk.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionNwTsk_Execute);
+            // 
+            // popupWindowShowActionPryctCncld
+            // 
+            this.popupWindowShowActionPryctCncld.AcceptButtonCaption = "Cancelar Proyecto";
+            this.popupWindowShowActionPryctCncld.CancelButtonCaption = null;
+            this.popupWindowShowActionPryctCncld.Caption = "Cancelar Proyecto";
+            this.popupWindowShowActionPryctCncld.Category = "RecordEdit";
+            this.popupWindowShowActionPryctCncld.ConfirmationMessage = null;
+            this.popupWindowShowActionPryctCncld.Id = "ce364e00-38b0-4f8e-b4c1-d51df5e06529";
+            this.popupWindowShowActionPryctCncld.ToolTip = null;
+            this.popupWindowShowActionPryctCncld.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionPryctCncld_CustomizePopupWindowParams);
+            this.popupWindowShowActionPryctCncld.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionPryctCncld_Execute);
+            // 
+            // simpleActionImprmrCtzcn
+            // 
+            this.simpleActionImprmrCtzcn.Caption = "Imprimir Cotización";
+            this.simpleActionImprmrCtzcn.Category = "Print";
+            this.simpleActionImprmrCtzcn.ConfirmationMessage = "Está seguro de Imprimir la Cotización?";
+            this.simpleActionImprmrCtzcn.Id = "ca284f30-7513-4cf7-8215-01d924030a4b";
+            this.simpleActionImprmrCtzcn.ToolTip = null;
+            this.simpleActionImprmrCtzcn.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionImprmrCtzcn_Execute);
+            // 
+            // simpleActionEdtrCtzcn
+            // 
+            this.simpleActionEdtrCtzcn.Caption = "Editar Cotización";
+            this.simpleActionEdtrCtzcn.Category = "Export";
+            this.simpleActionEdtrCtzcn.ConfirmationMessage = "Está seguro de Editar la Cotización ?";
+            this.simpleActionEdtrCtzcn.Id = "d278c7af-fc6a-4202-a8de-4ac4ddb8dc72";
+            this.simpleActionEdtrCtzcn.ToolTip = null;
+            this.simpleActionEdtrCtzcn.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleActionEdtrCtzcn_Execute);
+            // 
+            // popupWindowShowActionEnvrCrr
+            // 
+            this.popupWindowShowActionEnvrCrr.AcceptButtonCaption = "Enviar";
+            this.popupWindowShowActionEnvrCrr.CancelButtonCaption = "Cancelar";
+            this.popupWindowShowActionEnvrCrr.Caption = "Enviar Correo";
+            this.popupWindowShowActionEnvrCrr.Category = "PopupActions";
+            this.popupWindowShowActionEnvrCrr.ConfirmationMessage = null;
+            this.popupWindowShowActionEnvrCrr.Id = "990643c5-47f4-4d33-9afd-36b6fd4b8c3d";
+            this.popupWindowShowActionEnvrCrr.ToolTip = null;
+            this.popupWindowShowActionEnvrCrr.CustomizePopupWindowParams += new DevExpress.ExpressApp.Actions.CustomizePopupWindowParamsEventHandler(this.popupWindowShowActionEnvrCrr_CustomizePopupWindowParams);
+            this.popupWindowShowActionEnvrCrr.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.popupWindowShowActionEnvrCrr_Execute);
+            // 
+            // VCProyecto
+            // 
+            this.Actions.Add(this.simpleActionCxc);
+            this.Actions.Add(this.simpleActionAcptd);
+            this.Actions.Add(this.simpleActionCncld);
+            this.Actions.Add(this.simpleActionTrmnd);
+            this.Actions.Add(this.simpleActionEnPrcs);
+            this.Actions.Add(this.popupWindowShowActionNwTsk);
+            this.Actions.Add(this.popupWindowShowActionPryctCncld);
+            this.Actions.Add(this.simpleActionImprmrCtzcn);
+            this.Actions.Add(this.simpleActionEdtrCtzcn);
+            this.Actions.Add(this.popupWindowShowActionEnvrCrr);
+            this.ViewControlsCreated += new System.EventHandler(this.VCProyecto_ViewControlsCreated);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionCxc;
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionAcptd;
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionCncld;
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionTrmnd;
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionEnPrcs;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionNwTsk;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionPryctCncld;
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionImprmrCtzcn;
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleActionEdtrCtzcn;
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction popupWindowShowActionEnvrCrr;
+    }
+}
--- TCap/TCap.Module/Controllers/Proyectos/VCProyecto.cs
+++ TCap/TCap.Module/Controllers/Proyectos/VCProyecto.cs
@@ -1,1 +1,287 @@
+﻿using System;
+using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using TCap.Module.BusinessObjects.Proyectos;
+using TCap.Module.Utilerias;
+using TCap.Module.BusinessObjects.Empresa;
+using DevExpress.Data.Filtering;
+using DevExpress.XtraRichEdit;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
+namespace TCap.Module.Controllers.Proyectos
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class VCProyecto : ViewController
+    {
+        public VCProyecto()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+
+            TargetObjectType = typeof(Proyecto);
+            simpleActionCxc.TargetViewType = ViewType.DetailView;
+
+            simpleActionAcptd.TargetObjectType = typeof(Proyecto);
+            simpleActionAcptd.TargetViewType = ViewType.DetailView;
+
+            simpleActionCncld.TargetObjectType = typeof(Proyecto);
+            simpleActionCncld.TargetViewType = ViewType.DetailView;
+
+            simpleActionTrmnd.TargetObjectType = typeof(Proyecto);
+            simpleActionTrmnd.TargetViewType = ViewType.DetailView;
+
+            simpleActionEnPrcs.TargetObjectType = typeof(Proyecto);
+            simpleActionEnPrcs.TargetViewType = ViewType.DetailView;
+
+            popupWindowShowActionNwTsk.TargetObjectType = typeof(Proyecto);
+            popupWindowShowActionPryctCncld.TargetObjectType = typeof(Proyecto);
+
+            simpleActionImprmrCtzcn.TargetObjectType = typeof(Proyecto);
+            simpleActionImprmrCtzcn.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+            View.ObjectSpace.Committing += ObjectSpace_Committing;
+
+            simpleActionCxc.Active.SetItemValue("Visible", false);
+            simpleActionCncld.Active.SetItemValue("Visible", false);
+
+            simpleActionAcptd.TargetObjectsCriteria = "[Stts] == 'Prospecto' && !IsNewObject(This)";
+            simpleActionEnPrcs.TargetObjectsCriteria = "[Stts] == 'Aceptado' && [VvMrt] != 'Cancelado'";
+            simpleActionCxc.TargetObjectsCriteria = "[Stts] == 'Activo'";
+            simpleActionCncld.TargetObjectsCriteria = "!IsNewObject(This) && ([VvMrt] != 'Cancelado' && [Stts] != 'Terminado')";
+            simpleActionTrmnd.TargetObjectsCriteria = "[Stts] == 'EnProceso' && [VvMrt] != 'Cancelado'";
+
+            popupWindowShowActionNwTsk.TargetObjectsCriteria = "[VvMrt] != 'Cancelado' && [Stts] != 'Prospecto'";
+            popupWindowShowActionPryctCncld.TargetObjectsCriteria = "!IsNewObject(This) && ([VvMrt] != 'Cancelado' && [Stts] != 'Terminado')";
+        }
+
+        private void ObjectSpace_Committing(object sender, CancelEventArgs e)
+        {
+            if (View != null)
+            {
+                Proyecto doc = View.CurrentObject as Proyecto;
+
+                if (doc != null && View.ObjectSpace != null)
+                {
+                    doc.SttsAntrr = doc.Stts;
+                    if (View.ObjectSpace.IsNewObject(doc))
+                        Negocio.GrabaProyecto(doc);
+                }
+            }
+        }
+
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void simpleActionCxc_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+
+        }
+
+        private void VCProyecto_ViewControlsCreated(object sender, EventArgs e)
+        {
+            if (View != null)
+            {
+                Proyecto doc = View.CurrentObject as Proyecto;
+
+                if (View.ObjectSpace != null && View.ObjectSpace.IsNewObject(doc))
+                    Negocio.IniciaProyecto(doc as Proyecto);
+
+                /*
+                if (doc != null)
+                {
+                    doc.RemisionItems.ListChanged += RemisionItems_ListChanged;
+                }*/
+            }
+        }
+
+        private void simpleActionAcptd_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            if (View != null)
+            {
+                Proyecto doc = View.CurrentObject as Proyecto;
+
+                // doc.Stts = EProyectoStatus.Aceptado;
+                Negocio.ProyectoAceptado(doc);
+                View.ObjectSpace.CommitChanges();
+            }
+        }
+
+        private void simpleActionCncld_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            if (View != null)
+            {
+                Proyecto doc = View.CurrentObject as Proyecto;
+
+                doc.SttsAntrr = doc.Stts;
+                doc.VvMrt = EEstadoDcmntPryct.Cancelado;
+            }
+        }
+
+        private void simpleActionTrmnd_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            if (View != null)
+            {
+                Proyecto doc = View.CurrentObject as Proyecto;
+
+                doc.Stts = EProyectoStatus.Terminado;
+            }
+        }
+
+        private void simpleActionEnPrcs_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            if (View != null)
+            {
+                Proyecto doc = View.CurrentObject as Proyecto;
+
+                doc.Stts = EProyectoStatus.EnProceso;
+            }
+        }
+
+        private void popupWindowShowActionNwTsk_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            AgregarActividad newObj;
+
+            newObj = objectSpace.FindObject<AgregarActividad>(null);
+            if (newObj != null)
+                objectSpace.Delete(newObj);
+
+            newObj = objectSpace.CreateObject<AgregarActividad>();
+            e.View = Application.CreateDetailView(objectSpace, "AgregarActividad_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionNwTsk_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            AgregarActividad ha = e.PopupWindowViewCurrentObject as AgregarActividad;
+
+            if (View != null)
+            {
+                IObjectSpace os = View.ObjectSpace;
+                Proyecto obj = View.CurrentObject as Proyecto;
+
+                Actividad act = os.CreateObject<Actividad>();
+
+                act.Asnt = ha.Asnt;
+                if (ha.Asgnd != null)
+                    act.Asgnd = os.FindObject<EmpleadoProyecto>(new BinaryOperator("Oid", ha.Asgnd.Oid));
+                act.HrsEstmds = ha.HrsEstmds;
+                act.Prrdd = ha.Prrdd;
+                act.TpP = ha.TpP;
+                act.FchVncmnt = ha.FchVncmnt;
+                if (ha.FchVncmnt != null)
+                    act.RemindIn = TimeSpan.FromMinutes(1);
+                act.Nts = ha.Nts;
+
+
+                obj.Tareas.Add(act);
+                os.CommitChanges();
+            }
+        }
+
+        private void popupWindowShowActionPryctCncld_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            ProyectoCancelado newObj = objectSpace.FindObject<ProyectoCancelado>(null);
+
+            // TIT 2021 Mrz no me deja reusar un mismo objeto
+            if (newObj != null)
+                objectSpace.Delete(newObj);
+
+
+            newObj = objectSpace.CreateObject<ProyectoCancelado>();
+            e.View = Application.CreateDetailView(objectSpace, "ProyectoCancelado_DetailView", true, newObj);
+        }
+
+        // TIT Mrz 2021 No recuerdo por qué tengo dos acciones 
+        // que 'aparentemente' hacen lo mismo, dejaré sólo esta
+        private void popupWindowShowActionPryctCncld_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            ProyectoCancelado ha = e.PopupWindowViewCurrentObject as ProyectoCancelado;
+
+            if (View != null)
+            {
+                IObjectSpace os = e.PopupWindowView.ObjectSpace;
+                Proyecto obj = os.FindObject<Proyecto>(new BinaryOperator("Oid",
+                    (View.CurrentObject as Proyecto).Oid));
+
+                obj.SttsAntrr = obj.Stts;
+                obj.VvMrt = EEstadoDcmntPryct.Cancelado;
+                obj.FchCnclcn = ha.FchCnclcn;
+                obj.MtvCnclcnP = ha.MtvCnclcnP;
+                obj.Cnclcn = ha.Cnclcn;
+
+                os.CommitChanges();
+            }
+        }
+
+        private void simpleActionImprmrCtzcn_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            Proyecto obj = View.CurrentObject as Proyecto;
+            RichEditDocumentServer server = new RichEditDocumentServer();
+            List<Proyecto> datos = new List<Proyecto>();
+            datos.Add(obj);
+
+            server.LoadDocument("Cotizacion.docx", DocumentFormat.Doc);
+
+            DevExpress.XtraPrinting.PdfExportOptions pdf = new DevExpress.XtraPrinting.PdfExportOptions();
+
+            FieldOptions fieldOptions = server.Options.Fields;
+            fieldOptions.HighlightMode = FieldsHighlightMode.Auto;
+            fieldOptions.HighlightColor = System.Drawing.Color.LightSalmon;
+            fieldOptions.ThrowExceptionOnInvalidFormatSwitch = true;
+
+            // pdf.PageRange = "1";
+            server.Options.MailMerge.ViewMergedData = true;
+            server.Options.MailMerge.DataSource = datos;
+            server.Document.Fields.Update();
+
+            string fn = string.Format("{0}.pdf", obj.Clv);
+            server.ExportToPdf(fn);
+            ViewFile(fn);
+        }
+
+        private void ViewFile(string fn)
+        {
+            Process process = new Process();
+
+            if (File.Exists(fn))
+            {
+                process.StartInfo.FileName = fn;
+                process.Start();
+                process.WaitForInputIdle();
+            }
+        }
+
+        private void simpleActionEdtrCtzcn_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            ViewFile("Cotizacion.docx");
+        }
+
+        private void popupWindowShowActionEnvrCrr_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            IObjectSpace objectSpace = Application.CreateObjectSpace();
+            ProyectoCancelado newObj = objectSpace.CreateObject<ProyectoCancelado>();
+            e.View = Application.CreateDetailView(objectSpace, "ProyectoCancelado_DetailView", true, newObj);
+        }
+
+        private void popupWindowShowActionEnvrCrr_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+
+        }
+    }
+}
